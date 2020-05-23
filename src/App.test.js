bb0be243a875813666e018b40d8c464905911897
mascarenhaswanja <wanjamarly.deoliveiramascarenhas@georgebrown.ca>
2020-05-23 11:06:29 -0400

    Second test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Form from "./form";
+import Joke from "./joke";
 
 test("sets the value to the upper version of the value", () => {
   render(<Form />);
@@ -9,4 +10,13 @@ test("sets the value to the upper version of the value", () => {
 
   fireEvent.change(upperInput, { target: { value: upper } });
   expect(upperInput.value).toEqual(upper.toUpperCase());
+});
+
+test("Joke component receives props and then render the text", () => {
+  const { getByTestId } = render(
+    <Joke text="The funniest joke of this year." />
+  );
+  expect(getByTestId("joke-text")).toHaveTextContent(
+    "The funniest joke of this year."
+  );
 });
