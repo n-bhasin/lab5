9b053dde39f994d0c40d1c4b15af618467ef21ae
n-bhasin <nkbhasin28@gmail.com>
2020-05-23 08:29:51 -0400

    test one

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Form from "./form";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("sets the value to the upper version of the value", () => {
+  render(<Form />);
+  const upperInput = screen.getByLabelText(/upper/i);
+  const upper = "stuff";
+
+  fireEvent.change(upperInput, { target: { value: upper } });
+  expect(upperInput.value).toEqual(upper.toUpperCase());
 });
